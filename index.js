55a334a33f66469850efe7d0635a9d2c8ca0b668
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-11-06 19:31:51 +0000

    [object object]

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+class Rectangle{
+    constructor(length, height){
+        this.length = length;
+        this.height = height;
+    }
 
-let length = 5;
-let height = 10;
-
-function calculateArea(length, height){
-    return length * height;
+    calculateArea(){
+    return this.length * this.height;
+    }
 }
+
+let randomRectangle = new Rectangle(10,40);
+document.getElementById('result').innerHTML = randomRectangle;
